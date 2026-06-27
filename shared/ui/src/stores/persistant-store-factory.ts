af43901e170f573fa52ea53b8f8833b70f06be1a
Martin Alexander Ramos Yampufé <alexanderamsyam@gmail.com>
2026-06-26 21:12:44 -0500

    chore: restore dependencies

---
+++ shared/ui/src/stores/persistant-store-factory.ts
@@ -0,0 +1,51 @@
+import { persistentJSON } from "@nanostores/persistent"
+import { useStore } from "@nanostores/react"
+import { type WritableAtom } from "nanostores"
+import { useCallback } from "react"
+
+// Cache
+const cache = new Map<string, WritableAtom<unknown>>()
+
+// Factory
+function getOrCreateStore<T>(key: string, initialValue: T): WritableAtom<T> {
+  let $store = cache.get(key) as WritableAtom<T> | undefined
+
+  if (!$store) {
+    $store = persistentJSON<T>(key, initialValue)
+
+    cache.set(key, $store)
+  }
+
+  return $store
+}
+
+// Hook
+export function usePersistentState<T>(
+  key: string,
+  initialValue: T | (() => T)
+): [T, (next: T | ((prev: T) => T)) => void] {
+  // Init value
+  const resolvedInitialValue =
+    typeof initialValue === "function"
+      ? (initialValue as () => T)()
+      : initialValue
+
+  // Store
+  const $store = getOrCreateStore(key, resolvedInitialValue)
+
+  // State
+  const value = useStore($store)
+  const setValue = useCallback(
+    (next: T | ((prev: T) => T)) => {
+      const resolved =
+        typeof next === "function"
+          ? (next as (prev: T) => T)($store.get())
+          : next
+
+      $store.set(resolved)
+    },
+    [$store]
+  )
+
+  return [value, setValue]
+}
